446a98b62fc876d0fc7e5727351d52893df65683
= <edwinsilvestre0101@gmail.com>
2022-07-21 10:32:37 -0400

    Tuve que agregar nuevas caracteristicas de proporsion de informacion y tambien de salida de ventana.

--- ES/main.js
+++ ES/main.js
@@ -16,15 +16,37 @@ document.addEventListener("keypress", (ev)=> {
     if(ev.keyCode === 13 || ev.key == "Enter") {
         const {value} = $textarea,
         isAdd = /add\$/i,
+        inform = /inform\$/i,
+        exit = /exit\$/i,
         Add = new Event("add");
-        if(isAdd.test(value)){
-            $textarea.dispatchEvent(Add);
-        }
+
+        if(isAdd.test(value)) $textarea.dispatchEvent(Add);
+
+        else if(inform.test(value)) return $textarea.value = 
+            `# Activity Register.
+
+            Este es un registrador de actividades que sirve para registrar actividades
+            basandose en un lenguage minimalista @Sober para registrar con fecha completa
+            cada una de las actividades.
+            
+            Tiene un ventana especial que te avisa de cualquier error que alla ocurrido.
+            por ejemplo si eliminas el arreglo predeterminado cuando se carga, te avisara de ese
+            error que ocurrio con una ventana responsive donde depura cualquier error.
+            
+            Los errores que se comete cuando escribes para registrar una nueva actividad
+            es cuando no sigues la sintaxis de el lenguage @Sober.
+            
+            Si ya manejas las terminales como CMD, powershell, gitbash, etc... se te sera familiar
+            escribir en @Sober para registrar actividades. Disfrutalo es codigo abierto!
+                            
+            --Edwin Silvestre--`;
+            
+        else if (exit.test(value)) window.close();
 
     }
-});
 
+});
 document.addEventListener("click", (ev)=>  {
    if(ev.target.matches(".changeClass")) document.getElementById("error").className = "hidden";
    else if(ev.target.matches(".delete")) Deleting(ev.target);
-});
+})
